4855ed6b27bd6fb737092d5cb5395c04d22af513
vintagegamingsystems <vintagegamingsystems@gmail.com>
2014-02-10 19:10:48 -0800

    Update centerMouseButtonReassign.sh

--- centerMouseButtonReassign.sh
+++ centerMouseButtonReassign.sh
@@ -1,5 +1,4 @@
-#! /bin/bash
-# I put this file in my ~/.config/autostart/ directory, under Mint 15.
+# I put this code in my /etc/bash.bashrc file, under Mint 15. This will be a system wide change.
 # Assigns the center mouse button the same function as the left mouse button.
 # I used this command on my Dell M6400 Laptop.
 idNum=$(xinput | grep "SynPS/2 Synaptics TouchPad" | awk '{print $6}' | cut -c4-)
